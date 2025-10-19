ff37e7ed817512f66a3dc6465f679c9d6b9d1140
Heny254 <henrytrinh2605@gmail.com>
2025-10-19 09:44:37 +0700

    capnhatlan1

--- script.js
+++ script.js
@@ -818,10 +818,10 @@ function animatePlanetSystem() {
 let galaxyAudio = null;
 
 function preloadGalaxyAudio() {
-  const audioSources = [ 'https://github.com/Heny254/music/raw/refs/heads/main/phong-zin-zin.mp3'
-
+  const audioSources = [ 'https://github.com/Heny254/music/raw/refs/heads/main/phong-zin-zin.mp3',
+    'https://github.com/Heny254/music/raw/refs/heads/main/em-khong-le-loi.mp3', 'https://github.com/Heny254/music/raw/refs/heads/main/Perfect%20.mp3',
+    
   ];
-
   const randomIndex = Math.floor(Math.random() * audioSources.length);
   const selectedSrc = audioSources[randomIndex];
 
